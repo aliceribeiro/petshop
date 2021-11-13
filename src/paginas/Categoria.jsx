d93fe2ca97d440969f796d1688bc1adbffa73027
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-11-13 14:31:54 -0300

    feat: :sparkles: cria e implementa subcategoria

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,21 +1,46 @@
-import React from "react";
-import { useParams, Route, useRouteMatch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, Route, useRouteMatch, Link, Switch } from "react-router-dom";
+import { busca } from "../api/api";
 import "../assets/css/blog.css";
 import ListaCategorias from "../components/ListaCategorias";
 import ListaPost from "../components/ListaPost";
+import SubCategoria from "./SubCategoria";
 
 const Categoria = () => {
   const { id } = useParams();
-  const { path } = useRouteMatch();
+  const { url, path } = useRouteMatch();
+  const [subcategorias, setSubcategorias] = useState([]);
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubcategorias(categoria.subcategorias);
+    });
+  }, [id]);
+
   return (
     <>
       <div className="container">
         <h2 className="titulo-pagina">Pet Notícias</h2>
       </div>
       <ListaCategorias />
-      <Route exact path={`${path}/`}>
-        <ListaPost url={`/posts?categoria=${id}`} />
-      </Route>
+      <ul className="lista-categorias container flex">
+        {subcategorias.map((subcategoria) => (
+          <li
+            className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+            key={subcategorias}
+          >
+            <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+          </li>
+        ))}
+      </ul>
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`} />
+        </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria url={`/posts?categoria=${id}`} />
+        </Route>
+      </Switch>
     </>
   );
 };
